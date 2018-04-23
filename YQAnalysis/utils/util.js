4029c4dc39d59fa40c70325b59fcefb2925ccad9
Luke <zhoumuzhe@outlook.com>
2018-04-23 22:03:48 +0800

    调用后台接口

--- YQAnalysis/utils/util.js
+++ YQAnalysis/utils/util.js
@@ -9,11 +9,35 @@ const formatTime = date => {
   return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
 
+function getData(keywords,scope) {
+  return new Promise(function (resolve, reject) {
+    wx.request({
+      url:'https://api.niucodata.com/api/vip/items/?access_token=df6cfe180cc1aae3faabeccccf3a716c705c410e',
+      data: {
+        'keywords':keywords,
+        'scope':scope
+      },
+      header: {
+        'Content-Type': 'application/json'
+      },
+      success: function (res) {
+        console.log("success");
+        resolve(res);
+      },
+      fail: function (res) {
+        reject(res);
+        console.log("failed");
+      }
+    })
+  })
+}
+
 const formatNumber = n => {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
 
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  getData:getData
 }
